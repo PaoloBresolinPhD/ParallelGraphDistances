2f32382811e636cacbc16f4872fcce1156436d42
Paolo B. PhD <paolo.bresolin.2@phd.unipd.it>
2025-02-27 16:13:50 +0100

    removed torch instruction, since no more necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 import json
-import torch
 from tumor_dataset import TumorDataset
 from utils import Utils
 from graph_distances import GraphDistances
@@ -11,14 +10,6 @@ if __name__ == '__main__':
     with open(sys.argv[1], 'r') as file:
         settings = json.load(file)
     
-    # set the device to use for tensor operations
-    device = Utils.get_device(settings['device'])
-    print(f"Using device: {device}")
-
-    # limit the cores used by torch
-    torch.set_num_threads(settings['max_n_cores'])
-    torch.set_num_interop_threads(settings['max_n_cores'])
-
     # load training data and create a TumorDataset object
     train_data = TumorDataset(settings['train_dataset'])
 
@@ -30,5 +21,5 @@ if __name__ == '__main__':
     train_data.sample_one_graph_per_patient(rd_seed=settings['random_seed'])
 
     # compute the tensor with the distances between all pairs of graphs in the training dataset
-    train_distances = GraphDistances.compute_distances(Utils.flatten_list_of_lists(train_data.to_dataset_DiGraphs()), GraphDistances.ancestor_descendant_dist).to(device)
+    train_distances = GraphDistances.compute_distances(Utils.flatten_list_of_lists(train_data.to_dataset_DiGraphs()), GraphDistances.ancestor_descendant_dist)
     
