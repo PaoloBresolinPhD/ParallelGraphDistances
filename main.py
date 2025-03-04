950cb761875b514bf2c03ec15ee1b80017118258
Paolo B. PhD <paolo.bresolin.2@phd.unipd.it>
2025-03-04 08:06:40 +0100

    removed some unused variables, updated some variable names and comments

--- main.py
+++ main.py
@@ -1,83 +1,92 @@
 import sys
 import json
 import numpy as np
-from tumor_graph import TumorGraph
 from tumor_dataset import TumorDataset
 from utils import Utils
 #from graph_distances import GraphDistances
 from mpi4py import MPI as MPI
-import mpi4py
 import pickle
 
 if __name__ == '__main__':
     
+    # initialize MPI variables
     ROOT_PROCESS = 0
     comm = MPI.COMM_WORLD
-    p = comm.Get_size()
-
-    print("N_proc is : "+ str(p) )
-
-
+    n_processes = comm.Get_size()
     rank = comm.Get_rank()
-    n_graphs = 0
 
-    if(rank == ROOT_PROCESS):
+    # print the number of processes
+    print(f"The number of processes is: {n_processes}")
+
+    # root node
+    if rank == ROOT_PROCESS:
+
         # load the input file with configuration parameters
         with open(sys.argv[1], 'r') as file:
             settings = json.load(file)
         
-        # load training data and create a TumorDataset object
-        train_data = TumorDataset(settings['dataset_path'])
+        # load training data and create a list of TumorGraph objects
+        dataset = TumorDataset(settings['dataset_path'])
+        dataset  = Utils.flatten_list_of_lists(dataset.to_dataset_DiGraphs())
         
-        train_data  = Utils.flatten_list_of_lists(train_data.to_dataset_DiGraphs())
-        
-        n_graphs = len(train_data)
+        # compute and print the number of graphs in the dataset
+        n_graphs = len(dataset)
+        print(f"The number of graphs in the dataset is {n_graphs}")
 
-        print(f"\n N_graphs is : {n_graphs}\n")
-
+        # initialize the array that will contain the ancestry set of each graph
         ancestry_sets = np.empty(shape = n_graphs, dtype = set)
 
-        # Create a serialized object 
+        # since we have an array of objects, we need to convert it into an array of bytes so that we can scatter it
+        # first, serialize each graph
+        pickled_graphs = [pickle.dumps(graph) for graph in dataset]
 
-        pickled_graphs = [pickle.dumps(graph) for graph in train_data]
+        # convert each graph into an array of bytes 
+        byte_graphs = [np.frombuffer(pickled_graph, dtype=np.uint8) for pickled_graph in pickled_graphs]
 
-        #Convert each graph into an array of bytes 
-        byte_graphs = [np.frombuffer(pickled_graph, dtype = np.uint8 ) for pickled_graph in pickled_graphs]
+        # compute an array with the size of each graph in terms of number of bytes
+        graph_sizes_in_bytes = np.array([arr.size for arr in byte_graphs], dtype=int)
 
-        # Sizes and Displacements
-        graph_sizes_in_bytes = np.array([ arr.size for arr in byte_graphs], dtype= int)
+        # compute the cumulative sum of the sizes of the graphs
+        cumulative_sum = np.cumsum(graph_sizes_in_bytes)
 
-        #Byte buffer dimensions for the processors
-        cs = np.cumsum(graph_sizes_in_bytes)
-
-        sizes = np.empty(p,dtype = int)
-        sizes[0] = cs[n_graphs //p -1]
-        for i in range(1, p):
-            sizes[i] = cs[(i + 1) * (n_graphs//p) - 1] - sizes[i-1]
+        # create an array with the number of bytes to be sent to each process
+        sizes = np.empty(n_processes, dtype=int)
+        sizes[0] = cumulative_sum[n_graphs // n_processes - 1]
+        for i in range(1, n_processes):
+            sizes[i] = cumulative_sum[(i + 1) * (n_graphs // n_processes) - 1] - sizes[i-1]
         
-        displacements = np.insert((sizes),0,0)[:-1] 
+        # compute the displacements
+        displacements = np.insert((sizes), 0, 0)[:-1] 
         
-        #print((displacements)) 
-        # COncatenate all bytes into one array
+        # concatenate all bytes into one array
         send_buffer = np.concatenate(byte_graphs)
+    
+    # all non-root processes
     else:
+
+        # no buffer must be sent
         send_buffer = None
-        sizes = np.empty(p, dtype = int)
+
+        # initialize the array that will contain the sizes
+        sizes = np.empty(n_processes, dtype=int)
+
+        # no displacements must be computed
         displacements = None 
     
-    #Broadcast sizes to all processes
-    comm.Bcast(sizes, root = ROOT_PROCESS)
+    # broadcast sizes to all processes
+    comm.Bcast(sizes, root=ROOT_PROCESS)
 
-    #Allocate receive buffers
+    # allocate receive buffers
     recv_buffer = np.empty(sizes[rank], dtype = np.uint8)
 
-    #Scatter serialized data 
+    # scatter the serialized data 
     comm.Scatterv([send_buffer, sizes, displacements, MPI.BYTE], recv_buffer, root = ROOT_PROCESS)
 
-    #Deserialize Received objs
+    # deserialize the received objects
     received_objects = pickle.loads(recv_buffer.tobytes()) # TODO: fix here
 
-    print(f"Rank {rank} received {len(received_objects)}")
+    # print the number of received objects
+    print(f"Rank {rank} received {len(received_objects)} graphs")
 
 
     # compute the tensor with the distances between all pairs of graphs in the training dataset
